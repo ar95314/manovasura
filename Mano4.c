0b0762a4cf65b1a355d3371b7085d844e92ed5c6
manogudboy <35652687+manogudboy@users.noreply.github.com>
2018-01-30 16:49:58 +0530

    Create Mano4.c

---
+++ Mano4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char m;
+clrscr;
+printf("Enter the charater");
+scanf("%c",&m);
+if(m>'a'&&m<'z')
+{
+printf("Alphabet");
+}
+else
+{
+printf("Not alphabet");
+}
+}
