516a42b5bd9fca10635f1d1e534e51945f4ffdb6
manogudboy <35652687+manogudboy@users.noreply.github.com>
2018-01-30 16:01:49 +0530

    Create Mano1.c

---
+++ Mano1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int m;
+printf("enter the value of m=");
+scanf("%d",&m);
+if (a==0)
+{
+printf("zero");
+}
+else if(m>0)
+{
+    printf("positive");
+}
+else
+{
+    printf("negative");
+}
+}
