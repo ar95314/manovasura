2cc9269010d873078eaae3628929c7e381dfcfd3
manogudboy <35652687+manogudboy@users.noreply.github.com>
2018-02-04 14:14:14 +0530

    Create Mano9.c

---
+++ Mano9.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,m,i,j;
+clrscr();
+printf("enter a value of n,m");
+scanf("%d,%d",&n,&k);
+for(i=1;i<=n;i++)
+{
+printf("%d",n);
+}
+fof(j=1;j<=m;j++)
+{
+sum=sum+j;
+}
+printf("%d",sum);
+return0;
+getch();
+}
