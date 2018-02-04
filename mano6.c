c23a11944f4375c16d681c5970e04b24ca0c997d
manogudboy <35652687+manogudboy@users.noreply.github.com>
2018-02-04 13:18:01 +0530

    Create mano6.c

---
+++ mano6.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,m;
+clrscr();
+printf("Enter a b m values");
+scanf("%d%d%d",&a,&b,&m);
+if(a>b)&(a>c)
+{
+printf("\n a is big");
+}
+else if(b>a)&(b>m)
+{
+printf("\n b is big");
+}
+else
+{
+printf("\n m is big");
+}
+getch();
+}
