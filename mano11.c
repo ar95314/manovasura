f9d9637e43a8228fba0186bc41dc0470a808cc57
manogudboy <35652687+manogudboy@users.noreply.github.com>
2018-02-08 15:40:34 +0530

    Create mano11.c

---
+++ mano11.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int m, i, flag;
+    printf("Enter a positive number\n");
+    scanf("%d",&n);
+    for(i=2; i<=m/2; ++i)
+    {
+        if(m%i==0)
+        {
+        flag==o;
+                  break;
+        }
+    }
+    if (flag==0)
+        printf("\nYES, %d is a prime number.",n);
+    else
+        printf("NO, %d is not a prime number.",n);
+    return 0;
+}
