9cdb8a230ffa9301c0860c5caf5a7d28f94975c4
manogudboy <35652687+manogudboy@users.noreply.github.com>
2018-02-09 15:51:45 +0530

    Create mano20.c

---
+++ mano20.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int m,n,i;
+printf("enter number:");
+scanf("%d",&n);
+printf("enter range");
+scanf("%d",&n);
+for(i=0;i<=n;++i)
+{
+printf("%d * %d =%d\n",m,i,m*i);
+getch();
+return 0;
+}
