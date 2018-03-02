e30e54e92792e976910b7fbef1c20dd8af3527b9
manogudboy <35652687+manogudboy@users.noreply.github.com>
2018-03-02 14:38:07 +0530

    Create mano44.c

---
+++ mano44.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int n,i,count=0;
+    scanf("%d",&n);
+    for(i=1;i<=10;i++)
+    {
+        if(n==i)
+        count=1;
+    }
+    if(count==1)
+    
+        printf("yes");
+        else
+        printf("no");
+    
+
+}
