95b82bdae370430b39f048a3806771c01251fda6
manogudboy <35652687+manogudboy@users.noreply.github.com>
2018-03-16 14:02:23 +0530

    Update mano58.c

--- mano58.c
+++ mano58.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include<conio.h>
-int main(void) 
+int main()
 {
 int x,y,temp;
 printf("\n enter two numbers to swap:");
@@ -14,5 +14,5 @@ y=temp;
 printf("\n after swapping:");
 printf("\nx=%d",x);
 printf("\ny=%d",y);
-return 0;
+
 }
