11725d9fa572fed9752fce051348b3188c93f6de
manogudboy <35652687+manogudboy@users.noreply.github.com>
2018-03-16 14:04:48 +0530

    Update mano59.c

--- mano59.c
+++ mano59.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include<conio.h>
-int main() {
+int main() 
+{
 	int a[10],max=-999,n,i;
 	scanf("%d",&n);
 	for(i=0;i<n;i++)
@@ -15,5 +16,5 @@ int main() {
 	    }
 	}
 	printf("%d",max);
-	return 0;
+
 }
